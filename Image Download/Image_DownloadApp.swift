a1c3091b0cb8e8ccff123d121ff900c970c73159
Ermanno Fissore <ermannofissore@gmail.com>
2022-09-28 18:13:37 +0200

    Initial Commit

---
+++ Image Download/Image_DownloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_DownloadApp.swift
+//  Image Download
+//
+//  Created by Ermanno Fissore on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Image_DownloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
